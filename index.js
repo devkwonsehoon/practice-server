a39c6a922087e2bb4a75c1e6ca5afa34ddf462c4
hoon <devkwonsehoon@naver.com>
2021-07-20 16:57:41 +0900

    [update] arrow function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * @desc ES6 grammar review
+ */
+
 // function getAgeAverage(personArray) {
 // 	var average = 0;
 //   var sum = 0;
@@ -20,13 +24,17 @@
 
 // console.log(getAgeAverage(personArray)); // 30.6
 
-const students = ["kwon", "se", "hoon"];
+// const students = ["kwon", "se", "hoon"];
 
 // for ( let i = 0; i < students.length; i++ ){
 //   console.log(students[i])
 // }
 
-// // == ES6 new for grammar ==
+
+/**
+ * @desc ES6 for
+ */
+
 // // python for a in array 같은 느낌
 // for (let student of students){
 //   console.log(student)
@@ -37,8 +45,51 @@ const students = ["kwon", "se", "hoon"];
 //   console.log(students[index])
 // }
 
-// ES6 Array 내장 함수
-// 배열 안에 있는 값들 하나하나를 콜백함수의 인자로 넣어줌
-students.forEach((student) => {
-  console.log(student)
-})
+// // ES6 Array 내장 함수
+// // 배열 안에 있는 값들 하나하나를 콜백함수의 인자로 넣어줌
+// students.forEach((student) => {
+//   console.log(student)
+// })
+
+/**
+ * @desc ES6 arrow function
+ */
+
+//  function hello() {
+// 	console.log("Hello function");
+// }
+
+// // 첫번째 arrow function
+// const arrowFunction = () => {
+// 	console.log("Hello arrow function");
+// }
+
+// // 두번째 arrow function
+// const arrowFunctionWithoutReturn = () => console.log("Hello arrow function without return");
+
+// hello(); // Hello function
+// arrowFunction(); // Hello arrow function
+// arrowFunctionWithoutReturn(); // Hello arrow function without return
+
+
+// arrow function and forEach example
+let personArray = [
+  {"name": "John Doe", "age": 20},
+  {"name": "Jane Doe", "age": 19},
+  {"name": "Fred Doe", "age": 32},
+  {"name": "Chris Doe", "age": 45},
+  {"name": "Layla Doe", "age": 37},
+];
+
+const getAgeAverage = (personArray) => {
+  let sum = 0;
+
+  personArray.forEach(person => {
+    sum += person['age']  
+  });
+
+  const avg = sum / personArray.length;
+  return avg
+}
+
+console.log(getAgeAverage(personArray));
