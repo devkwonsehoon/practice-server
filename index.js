de32ede0bc2959e2a3dd48cdfd6a9983ccb3755b
hoon <devkwonsehoon@naver.com>
2021-07-20 17:23:53 +0900

    [update] promise, async, await

--- index.js
+++ index.js
@@ -73,23 +73,71 @@
 
 
 // arrow function and forEach example
-let personArray = [
-  {"name": "John Doe", "age": 20},
-  {"name": "Jane Doe", "age": 19},
-  {"name": "Fred Doe", "age": 32},
-  {"name": "Chris Doe", "age": 45},
-  {"name": "Layla Doe", "age": 37},
-];
+// let personArray = [
+//   {"name": "John Doe", "age": 20},
+//   {"name": "Jane Doe", "age": 19},
+//   {"name": "Fred Doe", "age": 32},
+//   {"name": "Chris Doe", "age": 45},
+//   {"name": "Layla Doe", "age": 37},
+// ];
 
-const getAgeAverage = (personArray) => {
-  let sum = 0;
+// const getAgeAverage = (personArray) => {
+//   let sum = 0;
 
-  personArray.forEach(person => {
-    sum += person['age']  
+//   personArray.forEach(person => {
+//     sum += person['age']  
+//   });
+
+//   const avg = sum / personArray.length;
+//   return avg
+// }
+
+// console.log(getAgeAverage(personArray));
+
+
+/**
+ * @desc ES6 Promise
+ */
+//  const isReady = true;
+//  // 1. Producer
+//  const promise = new Promise((resolve, reject) => {
+//    console.log("Promise is created!");
+//    if (isReady) {
+//      resolve("It's ready");
+//    } else {
+//      reject("Not ready");
+//    }
+//  });
+ 
+ // 2. Consumer
+//  promise
+//    // promise에서 resolve가 될경우
+//    .then(messsage => {
+//      console.log(messsage);
+//    })
+//    // promise에서 reject가 될경우
+//    .catch(error => {
+//      console.error(error);
+//    })
+ 
+//  // Promise is created!
+//  // It's ready
+
+
+/**
+ * @desc ES6 async, await
+ * @기능 promise를 더 쉽게 만들고 코드의 가독성까지 좋게 만들어준다.
+ */
+
+async function f() {
+
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("완료!"), 1000)
   });
 
-  const avg = sum / personArray.length;
-  return avg
+  let result = await promise; // 프라미스가 이행될 때까지 기다림 (*)
+
+  console.log(result); // "완료!"
 }
 
-console.log(getAgeAverage(personArray));
+f();
