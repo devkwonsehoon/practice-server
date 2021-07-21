61a237210d98e93d9d0b309240f88ebbbd66b650
hoon <devkwonsehoon@naver.com>
2021-07-21 15:37:13 +0900

    [add] router 모듈화 및 분리

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const goodsRouter = require("./routes/goods");
+const userRouter = require("./routes/user");
+
+app.use('/goods', goodsRouter);
+app.use('/user', userRouter);
+
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
