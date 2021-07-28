e6b343f1f9a27069a778d4d63e32eac279ebbd1d
hoon <devkwonsehoon@naver.com>
2021-07-28 17:03:05 +0900

    [add] todo api

--- todo/app.js
+++ todo/app.js
@@ -17,6 +17,12 @@ router.get("/", (req, res) => {
   res.send("Hi!");
 });
 
+router.get("/todos", async (req, res) => {
+  const todos = await Todo.find().sort("-order").exec();
+
+  res.send({ todos });
+});
+
 // 명사같은 것들도 복수형으로 쓰는 것을 권장한다.
 router.post("/todos", async (req, res) => {
   const { value } = req.body;
@@ -31,6 +37,40 @@ router.post("/todos", async (req, res) => {
   await todo.save();
 
   res.send({ todo });
+});
+
+router.patch("/todos/:todoId", async (req, res) => {
+  const { todoId } = req.params;
+  const { order, value, done } = req.body;
+
+  const todo = await Todo.findById(todoId).exec();
+
+  if (order) {
+    const targetTodo = await Todo.findOne({ order }).exec();
+    if (targetTodo) {
+      targetTodo.order = todo.order;
+      await targetTodo.save();
+    }
+    todo.order = order;
+  } else if (value) {
+    todo.value = value;
+  } else if (done !== undefined) {
+    todo.doneAt = done ? new Date() : null;
+  }
+
+  await todo.save();
+
+  res.send({});
+});
+
+router.delete("/todos/:todoId", async (req, res) => {
+  const { todoId } = req.params;
+
+  await Todo.findByIdAndDelete(todoId).exec();
+
+  res.send({
+    message: "삭제 완료",
+  });
 });
 
 app.use("/api", bodyParser.json(), router);
