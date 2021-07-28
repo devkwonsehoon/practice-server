78316fc0975d9a8277326a02fcb8b174588d4a64
hoon <devkwonsehoon@naver.com>
2021-07-28 12:59:11 +0900

    [add] post todo api

--- todo/app.js
+++ todo/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const Todo = require("./models/todo");
 
 mongoose.connect("mongodb://localhost/todo-demo", {
   useNewUrlParser: true,
@@ -15,6 +16,23 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.send("Hi!");
 });
+
+// 명사같은 것들도 복수형으로 쓰는 것을 권장한다.
+router.post("/todos", async (req, res) => {
+  const { value } = req.body;
+  const maxOrderTodo = await Todo.findOne().sort("-order").exec();
+  let order = 1;
+
+  if (maxOrderTodo) {
+    order = maxOrderTodo.order + 1;
+  }
+
+  const todo = new Todo({ value, order });
+  await todo.save();
+
+  res.send({ todo });
+});
+
 app.use("/api", bodyParser.json(), router);
 app.use(express.static("./assets"));
 
